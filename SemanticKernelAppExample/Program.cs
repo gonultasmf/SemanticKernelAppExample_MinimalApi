288786bec6f2816cbbf6da77416bf009a9e13805
gonultasmf <gonultasmf@hotmail.com>
2025-06-10 21:40:10 +0300

    add filter

--- SemanticKernelAppExample/Program.cs
+++ SemanticKernelAppExample/Program.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel;
 using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,10 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddKernel()
-    .AddOllamaChatCompletion("deepseek-r1", "http://localhost:11434");
+    .AddOllamaChatCompletion("deepseek-r1", "http://localhost:11434")
+    .Plugins.AddFromType<CalculatorPlugin>();
 builder.Services.AddOpenApi();
 builder.Services.AddRequestTimeouts();
 builder.Services.AddHttpClient();
+builder.Services.AddSingleton<IFunctionInvocationFilter, LoggingFilter>();
+builder.Services.AddSingleton<IPromptRenderFilter, SafePromptFilter>();
+builder.Services.AddSingleton<IAutoFunctionInvocationFilter, EarlyTerminationFilter>();
 
 var app = builder.Build();
 
@@ -31,6 +37,59 @@ app.MapPost("/chat", async (IChatCompletionService chatCompletionService, ChatMo
     return response.ToString();
 }).WithRequestTimeout(TimeSpan.FromMinutes(10));
 
+app.MapGet("/add/{number1}/{number2}", async (Kernel kernel, int number1, int number2) =>
+{
+    var arguments = new KernelArguments
+    {
+        ["number1"] = number1,
+        ["number2"] = number2
+    };
+    var addResult = await kernel.InvokeAsync("CalculatorPlugin", "add", arguments);
+    return addResult.GetValue<int>();
+}).WithRequestTimeout(TimeSpan.FromMinutes(10));
+
 app.Run();
 
 public record ChatModel(string Input);
+
+public class CalculatorPlugin
+{
+    [KernelFunction("add")]
+    [Description("İki sayısal değer üzerinde toplama işlemi gerçekleştirir.")]
+    [return: Description("Toplam değeri döndürür.")]
+    public int Add(int number1, int number2)
+        => number1 + number2;
+}
+
+public sealed class LoggingFilter : IFunctionInvocationFilter
+{
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        Console.WriteLine($"Çalıştırılıyor = Eklenti Adı : {context.Function.PluginName} | Fonksiyon Adı : {context.Function.Name}");
+        await next(context);
+        Console.WriteLine($"Çalıştırıldı = Eklenti Adı : {context.Function.PluginName} | Fonksiyon Adı : {context.Function.Name}");
+    }
+}
+
+public sealed class SafePromptFilter : IPromptRenderFilter
+{
+    public async Task OnPromptRenderAsync(PromptRenderContext context, Func<PromptRenderContext, Task> next)
+    {
+        await next(context);
+        context.RenderedPrompt = "3 + 5 sonucu kaçtır?";
+    }
+}
+
+public sealed class EarlyTerminationFilter : IAutoFunctionInvocationFilter
+{
+    public async Task OnAutoFunctionInvocationAsync(AutoFunctionInvocationContext context, Func<AutoFunctionInvocationContext, Task> next)
+    {
+        await next(context);
+ 
+        var result = context.Result.GetValue<string>();
+        if (result == "desired result")
+        {
+            context.Terminate = true;
+        }
+    }
+}
